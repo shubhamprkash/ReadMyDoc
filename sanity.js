ce9bf1a4fa0980bb5f3db7d198ad14716dcacf5e
ShubhamPrakash <d1901.bca@gmail.com>
2022-03-25 17:18:49 +0530

    frontend error 2

--- sanity.js
+++ sanity.js
@@ -4,10 +4,10 @@ import {
     createClient,
 } from "next-sanity"; 
 
-export const config ={
+export const config = {
 
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-    projectId : process.env.NEXT_PIBLIC_SANITY_PROJECT_ID,
+    projectId : process.env.NEXT_PIBLIC_SANITY_PROJECT_ID = "q9v9t9zx",
     apiVersion: "2021-03-25",
 
     useCdn: process.env.NODE_ENV === "production",
