8d820b04afa607707dc7e73bf4fda892b51b526f
ShubhamPrakash <d1901.bca@gmail.com>
2022-03-25 15:17:10 +0530

    error config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
-import type { NextPage } from 'next'
+
 import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import {sanityClient, urlFor } from "../sanity";
+import { Post } from '../typing';
 
-const Home: NextPage = () => {
+interface Props{
+  posts: [Post];
+}
+
+export default function Home({posts} : Props){
+console.log(posts)
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
@@ -21,9 +28,25 @@ const Home: NextPage = () => {
   );
 }
 
-
-export default Home
-
 export const getServerSideProps = async () =>{
-  
-}
+    const query = `*[_type == "post" ]{
+      _id,
+      title,
+      author->{
+      name,
+      image
+    },
+       description,
+       mainImage,
+       slug
+    }`;
+
+    const posts = await sanityClient.fetch(query);
+
+    return{
+      props:{
+
+      }
+    }
+
+};
